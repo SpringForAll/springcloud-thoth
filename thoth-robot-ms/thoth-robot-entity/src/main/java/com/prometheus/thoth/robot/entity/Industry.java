dca3facd99dc603cde5cee74de2fb20952d102e4
liangliang1259 <liangliang1259@163.com>
2018-01-05 10:53:44 +0800

    fixed：修改robot-ms无法启动的bug

---
+++ thoth-robot-ms/thoth-robot-entity/src/main/java/com/prometheus/thoth/robot/entity/Industry.java
@@ -0,0 +1,22 @@
+package com.prometheus.thoth.robot.entity;
+
+public class Industry {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
